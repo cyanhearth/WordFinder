0051282e6443726d98f42cd22b3731aaa1a2c19a
Craig Whyte <cyanhearth@gmail.com>
2016-04-20 17:03:32 +0100

    Add swipe views and tabs to help screen and set up floating action button

--- app/src/main/java/com/example/cyanhearth/wordfinder/HelpActivity.java
+++ app/src/main/java/com/example/cyanhearth/wordfinder/HelpActivity.java
@@ -1,28 +1,104 @@
 package com.example.cyanhearth.wordfinder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 public class HelpActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    MyPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        setTitle("Help");
+
+        // create floating action button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         if (fab != null) {
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    Uri uri = Uri.parse("mailto:cyanhearth@gmail.com?subject=WordFinder%20App");
+                    intent.setData(uri);
+                    startActivity(intent);
+
                 }
             });
         }
+
+        // create swipe views
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new DemoFragment(), "How To Use");
+        adapter.addFragment(new DemoFragment(), "Examples");
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        if (viewPager != null) {
+            viewPager.setAdapter(adapter);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        if (tabLayout != null) {
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
     }
 
+    public static class DemoFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.demo_fragment, container, false);
+            return v;
+
+        }
+
+    }
+
+    public class  MyPagerAdapter extends FragmentPagerAdapter {
+        private final ArrayList<Fragment> fragments = new ArrayList<>();
+        private final ArrayList<String> fragmentTitles = new ArrayList<>();
+
+        public MyPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment frag, String title) {
+            fragments.add(frag);
+            fragmentTitles.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitles.get(position);
+        }
+    }
 }
